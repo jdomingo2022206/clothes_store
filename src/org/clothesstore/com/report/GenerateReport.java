9a70349575a0a5eb4e91f96136577f39f5112467
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2023-07-07 16:34:50 -0600

    Se borro todo

--- src/org/clothesstore/com/report/GenerateReport.java
+++
@@ -1,9 +0,0 @@
-package org.clothesstore.com.report;
-
-import java.util.HashMap;
-
-public class GenerateReport {
-    public static void mostrarReporte(String fileName, String reportName, HashMap parameters) {
-        // TODO: implement
-    }
-}
