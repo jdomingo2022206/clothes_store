b357a0daf23f75dc8ac85435d5b913bc6fcfadec
darriola2021670 <darriola-2021670@kinal.edu.gt>
2023-09-01 15:41:47 -0600

    Realice el HTML y el modelo DAO de mi entidad Usuario

--- Store/src/java/modelo/Usuario.java
+++ Store/src/java/modelo/Usuario.java
@@ -1,6 +1,7 @@
+package modelo;
+
 public class Usuario {
-    
-    private int idUsuario;
+     private int idUsuario;
     private String nombreUsuario;
     private String apellidoUsuario;
     private String usuario;
@@ -56,6 +57,4 @@ public class Usuario {
     public void setClave(String clave) {
         this.clave = clave;
     }
-    
-    
 }
