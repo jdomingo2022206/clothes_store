c3881f54a60225ec0f8967aa6338c0855eae6791
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2023-09-12 22:31:01 -0600

    Se cambió la lista en el controlador

--- Store/src/java/controlador/Controlador.java
+++ Store/src/java/controlador/Controlador.java
@@ -241,13 +241,13 @@ public class Controlador extends HttpServlet {
                     cliente.setDireccion(direccion);
                     cliente.setTelefono(telefono);
                     clienteDAO.agregar(cliente);
-                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=listar").forward(request, response);
+                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=Listar").forward(request, response);
                     break;
                 case "Editar":
                     codCliente = Integer.parseInt(request.getParameter("idCliente"));
                     Cliente c = clienteDAO.listarCodigoCliente(codCliente);
                     request.setAttribute("cliente", c);
-                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=listar").forward(request, response);
+                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=Listar").forward(request, response);
                     break;
                 case "Actualizar":
                     String nombree = request.getParameter("txtNombreCliente");
@@ -260,12 +260,12 @@ public class Controlador extends HttpServlet {
                     cliente.setTelefono(telefonoo);
                     cliente.setIdCliente(codCliente);
                     clienteDAO.actualizar(cliente);
-                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=listar").forward(request, response);
+                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=Listar").forward(request, response);
                     break;
                 case "Eliminar":
                     codCliente = Integer.parseInt(request.getParameter("idCliente")); //cambio la variable
                     clienteDAO.eliminar(codCliente); // cambio la variable
-                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=listar").forward(request, response);
+                    request.getRequestDispatcher("Controlador?menu=Cliente&accion=Listar").forward(request, response);
                     break;
             }
             request.getRequestDispatcher("Cliente.jsp").forward(request, response);
