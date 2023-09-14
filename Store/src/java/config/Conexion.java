ac99c38508a8e69528ae2be8a1cd215a50aa2c7a
Jonathan <jdomingo-2022206@kinal.edu.gt>
2023-09-14 10:45:09 -0600

    Proyecto Finalizado

---
+++ Store/src/java/config/Conexion.java
@@ -0,0 +1,26 @@
+package config;
+
+/**
+ *
+ * @author Jonwk._.19
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    Connection conexion;
+    
+    public Connection Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_TiendaRopa?useSSL=false", "root", "admin");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("No se pudo establecer la conexión");
+        }
+        return conexion;
+    }
+    
+}
---
+++ Store/src/java/modelo/Usuario.java
@@ -0,0 +1,63 @@
+package modelo;
+
+public class Usuario {
+    
+    private int idUsuario;
+    private String nombreUsuario;
+    private String apellidoUsuario;
+    private String usuario;
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombreUsuario, String apellidoUsuario, String usuario, String clave) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
+    }
+
+    public void setApellidoUsuario(String apellidoUsuario) {
+        this.apellidoUsuario = apellidoUsuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
